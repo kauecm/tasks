a9a9cf201f7c13e66dcf76d1b8dd6462b63721d4
kauecm <kauecmoraes@hotmail.com>
2026-05-22 23:04:11 -0300

    Feature - Leitura de abas do excel

--- src/main/java/com/automatizacao/tasks/service/LeituraXlsxService.java
+++ src/main/java/com/automatizacao/tasks/service/LeituraXlsxService.java
@@ -25,43 +25,49 @@ public class LeituraXlsxService {
                 FileInputStream fis = new FileInputStream(caminhoArquivo);
                 XSSFWorkbook workbook = new XSSFWorkbook(fis)
         ) {
+        	
+        	int numeroDeAbas = workbook.getNumberOfSheets();
+        	DataFormatter formatter = new DataFormatter();
+        	
+        	for (int a = 0; a < numeroDeAbas; a++) {
+        		XSSFSheet sheet = workbook.getSheetAt(a);
+        		
+        		// começa na linha 1 ignorando cabeçalho
+        		for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+        			
+        			Row row = sheet.getRow(i);
+        			
+        			if (row == null) {
+        				continue;
+        			}
+        			
+        			// ignora linhas totalmente vazias
+        			if (linhaVazia(row)) {
+        				continue;
+        			}
+        			
+        			TarefaDTO dto = new TarefaDTO(
+        					get(row, 0, formatter),
+        					get(row, 1, formatter),
+        					get(row, 2, formatter),
+        					get(row, 3, formatter),
+        					get(row, 4, formatter),
+        					get(row, 5, formatter),
+        					get(row, 6, formatter),
+        					get(row, 7, formatter),
+        					get(row, 8, formatter),
+        					get(row, 9, formatter),
+        					get(row, 10, formatter),
+        					get(row, 11, formatter),
+        					get(row, 12, formatter)
+        					);
+        			
+        			tarefas.add(dto);
+        		}
+        	}
 
-            XSSFSheet sheet = workbook.getSheetAt(0);
 
-            DataFormatter formatter = new DataFormatter();
 
-            // começa na linha 1 ignorando cabeçalho
-            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-
-                Row row = sheet.getRow(i);
-
-                if (row == null) {
-                    continue;
-                }
-
-                // ignora linhas totalmente vazias
-                if (linhaVazia(row)) {
-                    continue;
-                }
-
-                TarefaDTO dto = new TarefaDTO(
-                        get(row, 0, formatter),
-                        get(row, 1, formatter),
-                        get(row, 2, formatter),
-                        get(row, 3, formatter),
-                        get(row, 4, formatter),
-                        get(row, 5, formatter),
-                        get(row, 6, formatter),
-                        get(row, 7, formatter),
-                        get(row, 8, formatter),
-                        get(row, 9, formatter),
-                        get(row, 10, formatter),
-                        get(row, 11, formatter),
-                        get(row, 12, formatter)
-                );
-
-                tarefas.add(dto);
-            }
 
         } catch (IOException e) {
             e.printStackTrace();
